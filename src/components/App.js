7991b168a20943f44f805e7cd5a277b1626520f6
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2023-12-02 15:12:20 +0300

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import blogData from "../data/blog";
 import Header from "./Header"
 import About from "./About"
 import Article from "./Article"
+import ArticleList from "./ArticleList";
 
 console.log(blogData);
 
@@ -11,7 +12,7 @@ function App() {
     <div className="App">
       <Header name={blogData.name} />
       <About image={blogData.image} about={blogData.about} />
-      {/*<Article /> */}
+      <ArticleList posts={blogData.posts} />
     </div>
   );
 }
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,5 +1,21 @@
-function ArticleList() {
-
+function ArticleList({ posts }) {
+    return (
+        <>
+            <main>
+                <article>
+                    {posts.map((post) => {
+                        return (
+                            <>
+                                <h3>{post.title}</h3>
+                                <small>{post.date}</small>
+                                <p>{post.preview}</p>
+                            </>
+                        )
+                    })}
+                </article>
+            </main>
+        </>
+    )
 }
 
 export default ArticleList;
